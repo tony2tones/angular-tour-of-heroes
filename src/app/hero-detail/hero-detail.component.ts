2f09ecf208079547e9b7f48477d23628cc2c3a4c
Anthony <anthony.marque@gmail.com>
2019-05-24 11:26:02 +0200

    added hero-details component Added services to locate specific Hero

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 import { Hero } from '../hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,20 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
   
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroService : HeroService,
+    private location : Location
+  ) {}
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero);
   }
 
 }
