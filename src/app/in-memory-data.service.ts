6e50ee6a6dd37c9fd64ce067296cfd4293e29741
Anthony <anthony.marque@gmail.com>
2019-06-13 15:04:00 +0200

    update hero service

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root"
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 11, name: "Mr. Nice" },
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -5,20 +5,41 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Hero } from "../hero";
 import { HEROES } from "../mock-heroes";
 import { MessageService } from "../services/message.service";
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
   constructor(private http: HttpClient ,private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: Fetched Heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    )
   }
 
   getHero(id:number): Observable<Hero> {
-    this.messageService.add(`HeroService: Fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_=> this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
+  
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+private handleError<T>(opertaion = 'opertaion', result?: T) {
+    return (error:any): Observable<T> => {
+    this.log(`${opertaion} failed: ${error.message}`);
+    return of(result as T);
+    }
+  };
+ 
+
 }
